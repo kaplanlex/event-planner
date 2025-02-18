b82ac66a9166c37b21df15cd67f34ff82183d7d0
AlexKaplan <alexkaplan110606@gmail.com>
2025-02-18 14:23:25 +0200

    work for redux

---
+++ src/redux/operations.js
@@ -0,0 +1,23 @@
+// src/redux/operations.js
+
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
+
+export const fetchTasks = createAsyncThunk(
+    "tasks/fetchAll",
+    // Використовуємо символ підкреслення як ім'я першого параметра,
+    // тому що в цій операції він нам не потрібен
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get("/tasks");
+            // При успішному запиті повертаємо проміс із даними
+            return response.data;
+        } catch (e) {
+            // При помилці запиту повертаємо проміс
+            // який буде відхилений з текстом помилки
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
